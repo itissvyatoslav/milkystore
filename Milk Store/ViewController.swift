bb83783a70589b508c9fdc8b9a2e8d97a2dc9ca9
itissvyatoslav <itissvyatoslav@mail.ru>
2021-01-07 13:40:09 +0700

    Initial Commit

---
+++ Milk Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milk Store
+//
+//  Created by Святослав Шевченко on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
